458d33a52a5381c016df7cc4fb2642ca32d0de87
Andrew Usher <andrewusher00@gmail.com>
2020-08-05 00:31:10 -0500

    feat: add PR link to message

--- api/[name].js
+++ api/[name].js
@@ -4,12 +4,14 @@ const SLACK_CLIENT = new WebClient(process.env.SLACK_API_KEY)
 
 module.exports = async (req, res) => {
   const { name } = req.query
-  const { action } = req.body
+  const { action, pull_request: { html_url: siteURL } } = req.body
   await SLACK_CLIENT.chat.postMessage({
     channel: name,
     mrkdwn: true,
     text: `
-    # ${name} ${action}
+    PR ${action} for ${name}
+
+    URL: ${siteURL}
     `
   })
 
