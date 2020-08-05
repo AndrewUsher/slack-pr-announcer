0ea34e0b2e99e5544fb19d4f5fb406a538f2051a
Andrew Usher <andrewusher00@gmail.com>
2020-08-05 00:19:31 -0500

    feat: add title to message

--- api/[name].js
+++ api/[name].js
@@ -3,8 +3,13 @@ const { WebClient } = require('@slack/web-api')
 const SLACK_CLIENT = new WebClient(process.env.SLACK_API_KEY)
 
 module.exports = async (req, res) => {
+  const { name } = req.query
+  const { action } = req.body
   await SLACK_CLIENT.chat.postMessage({
-    channel: 'casual-cognitions', text: JSON.stringify(req.body || '')
+    channel: name,
+    text: `
+    # ${name} ${action}
+    `
   })
 
   res.json({
