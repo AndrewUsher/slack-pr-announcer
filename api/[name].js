94eeb4cba3b2e3abd32f9df4b7a5f5965862eaf7
Andrew Usher <andrewusher00@gmail.com>
2020-08-05 00:07:07 -0500

    feat: initial announcer functionality

--- api/[name].js
+++ api/[name].js
@@ -1,5 +1,12 @@
-module.exports = (req, res) => {
-  console.log(req.query)
+const { WebClient } = require('@slack/web-api')
+
+const SLACK_CLIENT = new WebClient(process.env.SLACK_API_KEY)
+
+module.exports = async (req, res) => {
+  await SLACK_CLIENT.chat.postMessage({
+    channel: 'casual-cognitions', text: JSON.stringify(req.body || '')
+  })
+
   res.json({
     test: 'test'
   })
