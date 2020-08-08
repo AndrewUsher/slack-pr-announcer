fbd94ba2804e6ebd2886aabe8145c27f641a4582
Andrew Usher <andrewusher00@gmail.com>
2020-08-08 15:11:05 -0500

    refactor(pr): remove body

--- api/pr/[name].js
+++ api/pr/[name].js
@@ -1,22 +1,16 @@
 const { WebClient } = require('@slack/web-api')
-const { stripMarkdown } = require('../../utils/stripMarkdown')
 
 const SLACK_CLIENT = new WebClient(process.env.SLACK_API_KEY)
 
 module.exports = async (req, res) => {
   const { name } = req.query
-  const { action, pull_request: { diff_url: diffUrl, html_url: siteURL, body } } = req.body
-
-  // Slack doesn't support markdown
-  const sanitizedMarkdownBody = stripMarkdown(body)
+  const { action, pull_request: { diff_url: diffUrl, html_url: siteURL } } = req.body
 
   await SLACK_CLIENT.chat.postMessage({
     channel: name,
     mrkdwn: true,
     text: `
     PR ${action} for ${name}
-
-    ${sanitizedMarkdownBody}
 
     Diff URL: ${diffUrl}
     URL: ${siteURL}
