09dcc38a0d781e72f53c5c42d0b2db4b9cfefd75
Andrew Usher <andrewusher00@gmail.com>
2020-08-08 14:46:26 -0500

    feat(pr): add pull request description to message

--- api/pr/[name].js
+++ api/pr/[name].js
@@ -4,12 +4,14 @@ const SLACK_CLIENT = new WebClient(process.env.SLACK_API_KEY)
 
 module.exports = async (req, res) => {
   const { name } = req.query
-  const { action, pull_request: { html_url: siteURL } } = req.body
+  const { action, pull_request: { html_url: siteURL, body } } = req.body
   await SLACK_CLIENT.chat.postMessage({
     channel: name,
     mrkdwn: true,
     text: `
     PR ${action} for ${name}
+
+    ${body}
 
     URL: ${siteURL}
     `
