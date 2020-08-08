08f6989ffe7b59ea9c91abee386cb189c1ba99fc
Andrew Usher <andrewusher00@gmail.com>
2020-08-08 15:00:48 -0500

    feat(pr): strip markdown from PR body

--- api/pr/[name].js
+++ api/pr/[name].js
@@ -1,17 +1,22 @@
 const { WebClient } = require('@slack/web-api')
+const { stripMarkdown } = require('../../utils/stripMarkdown')
 
 const SLACK_CLIENT = new WebClient(process.env.SLACK_API_KEY)
 
 module.exports = async (req, res) => {
   const { name } = req.query
   const { action, pull_request: { html_url: siteURL, body } } = req.body
+
+  // Slack doesn't support markdown
+  const sanitizedMarkdownBody = stripMarkdown(body)
+
   await SLACK_CLIENT.chat.postMessage({
     channel: name,
     mrkdwn: true,
     text: `
     PR ${action} for ${name}
 
-    ${body}
+    ${sanitizedMarkdownBody}
 
     URL: ${siteURL}
     `
