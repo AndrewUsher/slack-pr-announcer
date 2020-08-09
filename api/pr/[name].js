09005fe41990a199b2636d8d5f52143d6f3b6c8c
Andrew Usher <andrewusher00@gmail.com>
2020-08-09 08:55:06 -0500

    feat(pr): add title to message

--- api/pr/[name].js
+++ api/pr/[name].js
@@ -5,7 +5,7 @@ const SLACK_CLIENT = new WebClient(process.env.SLACK_API_KEY)
 
 export default async (req, res) => {
   const { name } = req.query
-  const { action, pull_request: { diff_url: diffUrl, html_url: siteURL, user: { login } } } = req.body
+  const { action, pull_request: { diff_url: diffUrl, html_url: siteURL, title, user: { login } } } = req.body
 
   if (!shouldPostMessage) {
     res.status(200).send('Not Applicable')
@@ -16,6 +16,8 @@ export default async (req, res) => {
     mrkdwn: true,
     text: `
     PR ${action} by ${login} for ${name}
+
+    ${title}
 
     Diff URL: ${diffUrl}
     URL: ${siteURL}
